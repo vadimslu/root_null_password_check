d57cecd2553c709471c9225d9bf2825a71b44ea1
VadimS <vadim.sluzky@sunlife.com>
2019-06-28 08:47:29 -0400

    rc0.1

--- root_passwd_chk.sh
+++ root_passwd_chk.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 ###This bash script will scan all the local images for null password CVE-2019-5021####
 echo "All following images will be scanned now"
-#docker_img=$(docker images --format "table {{.ID}}")
-#  echo $docker_img
-for container in `docker images --format "table {{.ID}}"`; do
+docker_img=$(docker images --format "table {{.ID}}")
+for container in $docker_img; do
          image_id=$(docker create $container)
          docker cp $image_id:/etc/shadow -> /tmp/tmpshadow
-         echo "this $container is checked"
+         echo "Following $container will be scanned now for null root password"
          passwd_check=$(cat /tmp/tmpshadow)
             if [[ $passwd_check = *"root:::0:::::"* ]]
                then
-                   echo "failure"
-                   echo "This $container is effected" >> /tmp/null_passwd.txt
+                   echo "Following $container was identified for potential null root password" >> /tmp/null_passwd.txt
+                           docker images | grep $container >> /tmp/fullname.txt
+                           echo "====================" >> /tmp/fullname.txt
                else
-                   echo "no nill password is detected in this $container"
+                   echo "NO null password was detected in following $container"
           fi
          docker rm $image_id
 done
